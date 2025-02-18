69e1c0cd5bc1f11085c74ce59442f9a912f589c0
vaibhav admane <vaibhavadmane07@gmail.com>
2025-02-18 19:10:24 +0530

    hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
+  <>
    <div className="text-zinc-200 text-xl font-semibold">
-    Turf booking System
-    Vaibahc adamne is xllkhbasri is k4i ahtusituesdjd 
-    <h1>Vaibhav chamar gandu</h1>
-    <h2>idshodnb</h2>
-    <h3>VAibahv hello chhanar</h3>
+     <p>hellio mayank</p>
    </div>
+  </>
   );
 }

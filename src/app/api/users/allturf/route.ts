14e839820252522bc957bee177411231f68dbdef
Hansrajsahu10 <hansrajsahuhansrajsahu10@gmail.com>
2025-02-20 09:55:10 +0530

    Get all turf

---
+++ src/app/api/users/allturf/route.ts
@@ -0,0 +1,19 @@
+import { connectToDatabase } from "@/app/lib/mongodb";
+import Turf from "@/app/model/turf";
+import mongoose from "mongoose";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    await connectToDatabase();
+    try {
+        
+const result=await Turf.find();
+
+return NextResponse.json({message:"All Turf",result},{status:201});
+
+
+    } catch (error) {
+        console.error("All turf are load to failed:", error);
+        return NextResponse.json({ error: "All turf are load to failed" }, { status: 500 });
+    }
+}
